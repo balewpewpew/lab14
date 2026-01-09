fac760b029c3a7b1ec3e78276dd5a327f6ad4109
balewpewpew <supapitchaitan.2006@gmail.com>
2026-01-09 17:28:11 +0700

    lab14_2.cpp completed!!

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -20,4 +20,34 @@ int main(){
 	return 0;
 }
 
-//Do not modify source code above this line 
+void showMatrix(const bool x[][N]){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            cout << x[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+
+void inputMatrix(double y[][N]){
+    for(int i=1;i<=N;i++){
+        cout << "Row " << i << ": ";
+        for(int j=0;j<N;j++){
+            cin >> y[i-1][j];
+        }
+    }
+}
+
+void findLocalMax(const double a[][N], bool b[][N]){
+    for(int i=1;i<N;i++){
+        for(int j=1;j<N;j++){
+            if(j==0||j==N-1||i==0||i==N-1){
+                b[i][j] = false;
+            }else{
+                if((a[i][j] >= a[i][j-1]) && (a[i][j] >= a[i+1][j]) && (a[i][j] >= a[i][j+1]) && (a[i][j] >= a[i-1][j])) b[i][j] = true;
+                else b[i][j] = false;
+            }
+        }
+    }
+}
+
